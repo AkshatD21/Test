18176aeedc55aed8473f2f09e101d285fec7d3cb
Akshat <akshatd212003@gmail.com>
2024-02-26 21:06:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Map from "./Map";
+// import Map from "./Map";
+import CustomMap from "./GoogleMap";
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <CustomMap />
     </div>
   );
 }
--- src/Map.jsx
+++ src/Map.jsx
@@ -8,7 +8,7 @@ import {
 
 const containerStyle = {
   width: "100%",
-  height: "400px",
+  height: "100vh",
 };
 
 const center = {
@@ -20,24 +20,53 @@ function Map() {
   const [response, setResponse] = React.useState(null);
   const [directions, setDirections] = React.useState(null);
   const [travelMode, setTravelMode] = React.useState("DRIVING");
+  const [origin, setOrigin] = React.useState("37.7749, -122.4194"); // Default origin
+  const [destination, setDestination] = React.useState("37.7878, -122.4376"); // Default destination
 
-  const directionsCallback = (res) => {
+  const directionsCallback = React.useCallback((res) => {
     if (res !== null) {
       setResponse(res);
     }
-  };
+  }, []);
 
-  const onTravelModeChange = (event) => {
+  const onTravelModeChange = React.useCallback((event) => {
     setTravelMode(event.target.value);
-  };
+  }, []);
 
   return (
-    <LoadScript googleMapsApiKey="UR_API_KEY">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+    <LoadScript googleMapsApiKey="KEY">
+      <div>
+        <label>
+          Origin:
+          <input
+            type="text"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          />
+        </label>
+        <label>
+          Destination:
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        </label>
+        <label>
+          Travel Mode:
+          <select value={travelMode} onChange={onTravelModeChange}>
+            <option value="DRIVING">Driving</option>
+            <option value="BICYCLING">Bicycling</option>
+            <option value="TRANSIT">Transit</option>
+            <option value="WALKING">Walking</option>
+          </select>
+        </label>
+      </div>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={50}>
         <DirectionsService
           options={{
-            destination: "37.7878, -122.4376", // Destination latitude and longitude
-            origin: "37.7749, -122.4194", // Origin latitude and longitude
+            destination: destination, // Use the state for destination
+            origin: origin, // Use the state for origin
             travelMode: travelMode,
           }}
           callback={directionsCallback}
